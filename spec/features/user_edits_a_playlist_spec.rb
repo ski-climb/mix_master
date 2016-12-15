782cd17867d6f5eaace7a41ee00e722d88363edc
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-12-15 10:02:07 -0700

    Remove comments from tests

--- spec/features/user_edits_a_playlist_spec.rb
+++ spec/features/user_edits_a_playlist_spec.rb
@@ -1,18 +1,3 @@
-
-# given some playlists with songs exist in the database
-# as a user
-# when I visit a playlist's show page
-# and I click 'Edit'
-# and I should see the songs on the playlist checked
-# and I should see the current name of the playlist
-# and I update the playlist name
-# and I select an additional song (checkbox)
-# and I unselect a checkbox
-# and I click 'Update Playlist'
-# then I should see the updated playlist name
-# and I should see the name of the newly added song
-# and I should not see the name of the removed song
-
 require 'rails_helper'
 
 RSpec.feature "Playlist" do
